ae8d353b43f0a00595b6c405f6b6bf82b9ae2199
faiqalizade <faiq.alizade.00@mail.ru>
2022-06-16 12:44:41 +0400

    fix: db ondelete sql CASCADE

--- database/sql/comments.sql
+++ database/sql/comments.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jun 15, 2022 at 07:40 AM
+-- Generation Time: Jun 16, 2022 at 08:44 AM
 -- Server version: 5.7.34
 -- PHP Version: 8.0.8
 
@@ -30,27 +30,10 @@ SET time_zone = "+00:00";
 CREATE TABLE `comments` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `parent_id` int(11) NOT NULL DEFAULT '0',
+  `parent_id` int(11) DEFAULT NULL,
   `text` text NOT NULL,
-  `created_at` datetime NOT NULL,
-  `comment` int(11) UNSIGNED DEFAULT NULL
+  `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `comments`
---
-
-INSERT INTO `comments` (`id`, `username`, `parent_id`, `text`, `created_at`, `comment`) VALUES
-(1, 'faigalizade', 0, 'Comment', '2022-06-15 06:20:09', NULL),
-(2, 'aliyev', 1, 'Test', '2022-06-15 06:20:45', NULL),
-(3, 'babayeff', 2, 'Test', '2022-06-15 06:21:04', NULL),
-(4, 'faigalizade', 0, 'Teeeee', '2022-06-15 06:34:31', NULL),
-(5, '12312', 4, '12312', '2022-06-15 07:28:36', NULL),
-(6, '12312', 4, '12312', '2022-06-15 07:29:40', NULL),
-(7, 'Testov', 1, 'Testov', '2022-06-15 07:29:55', NULL),
-(8, 'Testov', 1, 'Testov', '2022-06-15 07:30:25', NULL),
-(9, 'Testov', 4, 'Testov', '2022-06-15 07:31:12', NULL),
-(10, 'Testov', 3, 'Testov', '2022-06-15 07:32:41', NULL);
 
 --
 -- Indexes for dumped tables
@@ -60,7 +43,8 @@ INSERT INTO `comments` (`id`, `username`, `parent_id`, `text`, `created_at`, `co
 -- Indexes for table `comments`
 --
 ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `comments_parent_id_id` (`parent_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -70,7 +54,17 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_parent_id_id` FOREIGN KEY (`parent_id`) REFERENCES `comments` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
